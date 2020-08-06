bf0b81a0030afde8f323a5854b82af4091418ae1
Nyarlathotepss <rcp.nmillet@gmail.com>
2020-08-06 10:43:56 +0200

    first view and url

--- pitemp/pitemp/settings.py
+++ pitemp/pitemp/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'pitemp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],   # use a project-lvl templates directory,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
